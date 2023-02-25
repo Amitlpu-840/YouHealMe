01459d5cc7a3f612731895a482b8c874b8f13910
Amit Giri <thisisag840@gmail.com>
2023-02-25 12:17:14 +0530

    anonymous chat user added

--- client/src/index.js
+++ client/src/index.js
@@ -6,22 +6,23 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Music from './components/Music';
 import Home from './components/Home';
 import Communities from './components/Communities';
-import ChatAnonymus from './components/ChatAnonymus';
+import ChatAnonymus from './components/chatComponent/ChatAnonymus';
 import Activities from './components/Activities';
+// import ChatAreas from './components/ChatAreas';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-    <Routes>
-      <Route path='/' element={<App/>}>
-        <Route index element = {<Home/>}/>
-        <Route path='Music' element={<Music/>}/>
-        <Route path='Communities' element={<Communities/>}/>
-        <Route path='ChatAnonymous' element={<ChatAnonymus/>}/>
-        <Route path='Activities' element={<Activities/>}/>
-      </Route>
+      <Routes>
+        <Route path='/' element={<App />}>
+          <Route index element={<Home />} />
+          <Route path='Music' element={<Music />} />
+          <Route path='Communities' element={<Communities />} />
+          <Route path='ChatAnonymous' element={<ChatAnonymus />} />
+          <Route path='Activities' element={<Activities />} />
+        </Route>
       </Routes>
-    </BrowserRouter>    
+    </BrowserRouter>
   </React.StrictMode>
 );
